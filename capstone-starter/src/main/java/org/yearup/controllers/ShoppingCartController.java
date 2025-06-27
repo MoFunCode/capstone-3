d9839d35f71b0667e100c50455c68ffc1802ceec
MoFunCode <mohamed.sul02@gmail.com>
2025-06-26 23:38:46 -0500

    Add endpoint to add product to shopping cart

--- capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -3,13 +3,17 @@ package org.yearup.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
 
 import java.security.Principal;
@@ -52,7 +56,35 @@ public class ShoppingCartController {
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
 
+    @PostMapping("/products/{productId}")
+    public void addProductToCart(@PathVariable int productId, Principal principal) {
 
+        try {
+            String userName = principal.getName();
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            ShoppingCart cart = shoppingCartDao.getByUserId(userId);
+
+            if (cart.contains(productId)) { //Hashmapppp how cool
+                ShoppingCartItem existingItem = cart.get(productId);
+                existingItem.setQuantity(existingItem.getQuantity() + 1);
+            } else {
+                // Get the Product object from database first
+                Product product = productDao.getById(productId);
+
+                ShoppingCartItem newItem = new ShoppingCartItem();
+                newItem.setProduct(product);
+                newItem.setQuantity(1);
+                cart.add(newItem);
+            }
+
+            shoppingCartDao.save(userId, cart);
+
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
