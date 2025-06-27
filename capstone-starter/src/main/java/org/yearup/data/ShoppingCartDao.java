9d865a4acd462af3260211fa49a32e375b596217
MoFunCode <mohamed.sul02@gmail.com>
2025-06-26 23:22:09 -0500

    Add save and clearCartByUserId methods to ShoppingCartDao

--- capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,11 +1,13 @@
 package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
-import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
-    ShoppingCartItem getByUserIdAndProductId(int userId, int productId);
+    void save(int userId, ShoppingCart cart);
+    void clearCartByUserId(int userId);
 
 }
+
+
